9af9bd1dd099aae08f836ffdb53ac87127c7e641
Peter Kalinowski <petekal1323@gmail.com>
2025-03-06 10:56:10 -0500

    Completed Exercise 2 with math operations methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
+            /*Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
             
             Console.WriteLine("What is your favorite color?");
@@ -20,7 +20,52 @@
                         $"{name} really likes the color {color}. Also {name}'s favorite animal is a {animal}. " +
                         $"{name}'s favorite artist is {artist}. That's cool...... Welp, that's the end of the most pointless story, but hey we're practicing C#. I hope you enjoyed it.";
             
-            Console.WriteLine(story);
+            Console.WriteLine(story);*/
+            
+            
+            // Exercise 2
+            
+            Console.WriteLine(Add(2,3));
+            Console.WriteLine(Subtract(8,3));
+            Console.WriteLine(Multiply(2,3));
+            Console.WriteLine(Divide(15,3));
+            
+            sumNumbers(1,2,3,4,5,6,7,8,9,10);
+            Console.WriteLine(sumNumbers(1,2,3,4,5,6,7,8,9,10));
+        
+
         }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        
+        static int Divide(int a, int b)
+        {
+            return a / b;
+            
+        }
+        
+       public static int sumNumbers(params int[] numbers)
+       {
+           int sum = 0;
+           foreach (int number in numbers)
+           {
+               sum = sum + number;
+           }
+
+           return sum;
+       }
     }
 }
